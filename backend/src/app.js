7b893392a6e29e1e86b2679437cedde67987a8e9
Amit Kumar <161187431+Amit-Kumar63@users.noreply.github.com>
2025-03-10 12:55:07 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 
-// Reset Count after 1 week from limite model
+// Reset Count after 1 week from limit model
 cron.schedule('0 0 * * 0', async () => {
     try {
         await limitModel.updateMany({}, { count: 0 });
